5b0fa9e7228146f494a6b8174c660f64e709f673
Ondřej Pulc <ondra@Ondrejs-MacBook-Air.local>
2023-09-16 16:23:53 +0200

    :wrench: config for replicate

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python3
+
 import replicate
 
 training = replicate.trainings.create(
     version="stability-ai/sdxl:d830ba5dabf8090ec0db6c10fc862c6eb1c929e1a194a5411852d25fd954ac82",
     input={
-        "input_images": "https://my-domain/my-input-images.zip",
+        "input_images": "https://raw.githubusercontent.com/exezbcz/Woho/main/train.zip",
+	"caption_prefix": 'In the style of retro,',
     },
-    destination="my-name/my-model"
+    destination="exezbcz/sdxl-retro"
 )
 print(training)
